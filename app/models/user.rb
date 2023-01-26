0b104649fffd76b14b1ffebf895856e95ec97cfe
sinzanrin <niiyama.business@gmail.com>
2023-01-26 17:29:09 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-  validates :family_name, 
-  validates :first_name, 
+  validates :family_name
+  validates :first_name 
   end
 
   validates :family_name_kana, presence: true,

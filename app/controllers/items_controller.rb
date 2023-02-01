4a9f9e67aa40a5a491a7e21dfb79eda1eeaf6fe6
sinzanrin <niiyama.business@gmail.com>
2023-02-01 20:20:59 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :together_item, only: [:show, :edit, :update]
+  before_action :together_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -35,6 +35,13 @@ class ItemsController < ApplicationController
     else
       render 'edit' 
     end   
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
   private

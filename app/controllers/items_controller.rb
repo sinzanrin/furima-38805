32b8bcf5dccde828f72fb1b8f064bc5d9307ce9b
sinzanrin <niiyama.business@gmail.com>
2023-02-01 20:30:58 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,8 +40,8 @@ class ItemsController < ApplicationController
   def destroy
     if @item.user_id == current_user.id
       @item.destroy
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 
   private

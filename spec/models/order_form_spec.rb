50d50c116ab43fce73b393f46873e373627c6eaf
sinzanrin <niiyama.business@gmail.com>
2023-02-09 15:29:38 +0900

    修正

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
     before do
-      @order_form = FactoryBot.build(:order_form)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.build(:item)
+      @order_form = FactoryBot.build(:order_form, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do

4de055d78ab4bf14b618ce280a2ea0df784bb12a
sinzanrin <niiyama.business@gmail.com>
2023-02-09 15:35:12 +0900

    修正

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe OrderForm, type: :model do
     before do
       user = FactoryBot.create(:user)
-      item = FactoryBot.build(:item)
+      item = FactoryBot.create(:item)
       @order_form = FactoryBot.build(:order_form, user_id: user.id, item_id: item.id)
   end
 
